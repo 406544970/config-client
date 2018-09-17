230af29d0ff7babe58b947cab61492ce393cf1df
flyman <406544970@qq.com>
2018-09-17 10:22:19 +0800

    11

--- src/main/java/com/forezp/configclient/ConfigClientApplication.java
+++ src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -1,15 +1,12 @@
 package com.forezp.configclient;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@EanbleConfigServer
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(ConfigClientApplication.class, args);
 		new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
 
 	}
